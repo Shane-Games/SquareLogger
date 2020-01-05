34cce65b715eec86c4c83c27d0c4cbc708778cb5
Shane_Games <45543346+Shane-Games@users.noreply.github.com>
2020-01-05 16:20:21 -0500

    Version 1.10

--- SquareLogger.js
+++ SquareLogger.js
@@ -1,15 +1,19 @@
 const fs = require("fs");
 const stream = fs.createWriteStream("app.log")
 
-module.exports.logInfo = function(infoMsg) {
-    stream.write("[Info]: " + infoMsg);
+module.exports.logInfo = function (infoMsg) {
+    stream.write("[INFO]: " + infoMsg);
     stream.end();
 }
-module.exports.logInfo = function(warnMsg) {
-    stream.write("[Warn]: " + warnMsg);
+module.exports.logWarn = function (warnMsg) {
+    stream.write("[WARN]: " + warnMsg);
     stream.end();
 }
-module.exports.logError = function(errorMsg) {
-    stream.write("[Error]: " + errorMsg);
+module.exports.logError = function (errorMsg) {
+    stream.write("[ERROR]: " + errorMsg);
+    stream.end();
+}
+module.exports.logDebug = function (debugMsg) {
+    stream.write("[DEBUG]: " + debugMsg);
     stream.end();
 }
